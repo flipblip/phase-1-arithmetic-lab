98302cc7899f0a4c940401eedb9393b22ec49b21
fliblip <wickynjy@gmail.com>
2022-08-24 12:22:43 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,2 +1,39 @@
+// function multiply(num1 = 31, num2 = 2){
+//     return num1 * num2;
+// }
 
-// Write your code here
+
+// function random(Number){
+//     return Math.random();
+// }
+
+// function calculateRemainder(remainder){
+//     return num3%num4;
+// }
+
+// // function max(){
+// //     return Math.max(2, 15, 12, 20)
+// // }
+
+// // console.log(returnMax);
+
+// function max(){
+//     const set1 = ([2, 10, 20]);
+//     return Math.max(...set1)
+// }
+
+
+const num1 = 31;
+const num2 = 2;
+
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() *10) +1
+
+const num3 = 20;
+const num4 = 16;
+
+const mod = num3%num4;
+
+const max = Math.max(5, 6, 18, 20)
+
